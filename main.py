43c0e5dd7d2550d625828c9b06d3f9c4573eb3b7
systemnaut <57674086+systemnaut@users.noreply.github.com>
2020-11-18 05:35:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-##Copyright (C) 2020  torswq(systemnaut)
+##Copyright (C) 2020  Aitor Santoro
 ##
 ##    This program is free software: you can redistribute it and/or modify
 ##    it under the terms of the GNU General Public License as published by
